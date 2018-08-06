428fd6698682f85d4f620548bda29a3fff9028fe
ckrgithub <13560177212@163.com>
2018-08-06 23:01:46 +0800

    多个lib引用butterknife

--- app/src/main/java/com/ckr/vibrato/MainFragment.java
+++ app/src/main/java/com/ckr/vibrato/MainFragment.java
@@ -3,31 +3,38 @@ package com.ckr.vibrato;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+
+import com.ckr.baseframework.BaseFrameWorkFragment;
+import com.ckr.pageview.view.PageView;
+
+import butterknife.BindView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends BaseFrameWorkFragment {
+
+	@BindView(R.id.pageView)
+	PageView pageView;
 
 	public static MainFragment newInstance() {
-		
+
 		Bundle args = new Bundle();
-		
+
 		MainFragment fragment = new MainFragment();
 		fragment.setArguments(args);
 		return fragment;
 	}
 
+	@Override
+	protected void init() {
+
+	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_main, container, false);
+	protected int getLayoutId() {
+		return R.layout.fragment_main;
 	}
 
 }
