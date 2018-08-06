e22021e9fb0ee760a2ee09e8d06e97de2d8f1d07
ckrgithub <13560177212@163.com>
2018-08-06 22:29:59 +0800

    搭建快速开发的framework lib

--- app/src/main/java/com/ckr/vibrato/MainActivity.java
+++ app/src/main/java/com/ckr/vibrato/MainActivity.java
@@ -1,13 +1,25 @@
 package com.ckr.vibrato;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction transaction = fragmentManager.beginTransaction();
+		if (savedInstanceState == null) {
+			transaction.add(R.id.main_container, MainFragment.newInstance(), MainFragment.class.getSimpleName()).commit();
+		} else {
+			Fragment fragment = fragmentManager.findFragmentByTag(MainFragment.class.getSimpleName());
+			transaction.show(fragment).commit();
+		}
 	}
+
 }
